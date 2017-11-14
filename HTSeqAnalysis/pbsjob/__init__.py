cf25828bbd5da004060aedb72e4d89c81f3fddd3
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-13 20:01:03 -0600

    Changed the name of the package

---
+++ HTSeqAnalysis/pbsjob/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .qstat import Qstat
+from .pbsjob import PBSJob
+from .pbsutils import basejobids, writecodefile, import_temp, file2str
--- HTSeqAnalysis/run_htseq.py
+++ HTSeqAnalysis/run_htseq.py
@@ -6,8 +6,8 @@ df = pd.read_csv('impulsivity_samplenames.csv', header=None)
 samplenames = sorted(list(df[0]))
 
 path = '/ddn/home3/vallender/Projects/Impulsivity'
-command = '/home/ums/r1954/ptmp/HTSeq-0.9.1/scripts/htseq-count'
-
+commandpath = '/home/ums/r1954/ptmp/HTSeq-0.9.1/scripts/htseq-count'
+command = 'htseq-count'
 
 for samplename in samplenames:
     samplepath = os.path.join(path, samplename)
