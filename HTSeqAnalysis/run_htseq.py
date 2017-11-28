a5fe3479eec33a689cf0978ac9060b2db33466e0
sdhutchins <sdhutchins@users.noreply.github.com>
2017-11-28 13:29:20 -0600

    Added csvtolist util to run_htseq.py

--- HTSeqAnalysis/run_htseq.py
+++ HTSeqAnalysis/run_htseq.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
 import os
-from HTSeqAnalysis.pbsjob import PBSJob
 
-df = pd.read_csv('impulsivity_samplenames.csv', header=None)
-samplenames = sorted(list(df[0]))
+from HTSeqAnalysis.utils import csvtolist
 
-path = '/ddn/home3/vallender/Projects/Impulsivity'
-commandpath = '/home/ums/r1954/ptmp/HTSeq-0.9.1/scripts/htseq-count'
-command = 'htseq-count'
 
-for samplename in samplenames:
-    samplepath = os.path.join(path, samplename)
+def main(samplesfile, path):
+    samplenames = csvtolist(samplesfile)
+    command = 'htseq-count'
+
+    for samplename in samplenames:
+        samplepath = os.path.join(path, samplename)
+        print(samplepath)
