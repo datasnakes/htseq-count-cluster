991a976fac3315de2c97cbb0e480f806999d5736
sdhutchins <sdhutchins@outlook.com>
2017-11-28 15:21:10 -0600

    updated run_htseq.py

--- HTSeqAnalysis/run_htseq.py
+++ HTSeqAnalysis/run_htseq.py
@@ -4,10 +4,14 @@ import os
 from HTSeqAnalysis.utils import csvtolist
 
 
-def main(samplesfile, path):
+def main(samplesfile, outpath):
     samplenames = csvtolist(samplesfile)
     command = 'htseq-count'
 
     for samplename in samplenames:
-        samplepath = os.path.join(path, samplename)
+        samplepath = os.path.join(outpath, samplename)
         print(samplepath)
+
+
+if __name__ == '__main__':
+    main(samplesfile='impulsivity_samplenames.csv', outpath=os.getcwd())
