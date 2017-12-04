c1769aeba1e6ccf06f6ed4b25e475a8fe1f71bef
sdhutchins <sdhutchins@users.noreply.github.com>
2017-12-04 13:49:40 -0600

    Added merge.py which merges counts tables.

---
+++ HTSeqAnalysis/merge.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import os
+
+
+def merge_counts_tables(filesdirectory):
+    """Merge multiple counts tables into 1 counts table.
+
+    After running htseq-count-cluster, there will be a counts table for each
+    sample in the output directory. This function will use the genes column as
+    the first column and then insert each subsequent sample name as column
+    header with counts data as the column rows.
+    """
+    files = os.listdir(filesdirectory)
+
+    samplenames = []
+    sample_dfs = []
+
+    for file in files:
+        filename, ext = file.split('.')
+        samplename, barcode = filename.split('-')
+        samplenames.append(samplename)
+        filep = os.path.join(filesdirectory, file)
+        data = pd.read_table(filep, header=None, names=['Genes', samplename])
+        sdf = 'df_' + samplename
+        sdf = pd.DataFrame(data=data)
+        sample_dfs.append(sdf[samplename])
+        genes = list(sdf['Genes'])
+
+    genes_df = pd.DataFrame(genes, columns=['Genes'])
+    samples_df = pd.concat(sample_dfs, axis=1)
+    final_df = pd.concat([genes_df, samples_df], axis=1)
+
+    final_df.to_csv('merged_counts_table.csv', index=False)
