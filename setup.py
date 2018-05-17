bd9fec55c6a7f763b9496e11c78c8d57875f4cca
S. Hutchins <ms.s.hutchins@gmail.com>
2018-05-17 11:20:33 -0500

    Removed 3.7 from setup.py and travis.yml

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6'
         ],
     project_urls={
             'Documentation': 'https://tinyurl.com/yb7kz7zz',
