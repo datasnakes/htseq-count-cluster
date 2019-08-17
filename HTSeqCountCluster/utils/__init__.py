d0a658892311401e17c71cfaacd612a85faac16c
Shaurita <ms.s.hutchins@gmail.com>
2019-08-16 19:23:23 -0500

    Added docstring to csvtolist utils function.

--- HTSeqCountCluster/utils/__init__.py
+++ HTSeqCountCluster/utils/__init__.py
@@ -2,8 +2,17 @@
 import pandas as pd
 
 
-def csvtolist(csvfile):
+def csvtolist(csvfile, column=0):
+    """Convert a column of a csv file to a list.
+
+    :param csvfile: A comma delimited file.
+    :type csvfile: str
+    :param column: The number of the column to convert.
+    :type column: int
+    :return: A list
+    :rtype: list
+    """
     df = pd.read_csv(csvfile, header=None)
-    output_list = sorted(list(df[0]))
+    output_list = sorted(list(df[column]))
 
     return output_list
